5baea931752846fd5409c40ef0d8e9ac018f9a2a
Dylan Garrett <dylang@microsoft.com>
2023-02-10 12:36:51 -0800

    Remove some hacks

--- controllers/machine.go
+++ controllers/machine.go
@@ -313,9 +313,9 @@ func firstPartitionFromDevice(device string) string {
 	switch {
 	case nvmeDevice.MatchString(device), emmcDevice.MatchString(device):
 		// TODO: make this dynamic
-		return fmt.Sprintf("%sp2", device)
+		return fmt.Sprintf("%sp1", device)
 	default:
-		return fmt.Sprintf("%s2", device)
+		return fmt.Sprintf("%s1", device)
 	}
 }
 
